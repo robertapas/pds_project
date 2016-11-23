de05a69ba9ad96e1d76c804672e13ce00ce01841
robertapas <roberta.pas92@gmail.com>
2016-11-23 18:12:01 +0100

    Revert "login ok"
    This reverts commit 6aaaa208f558a5ca53065fede2979ae63e9fc9e4.

--- clientWpf/clientWpf/MainWindow.xaml.cs
+++ clientWpf/clientWpf/MainWindow.xaml.cs
@@ -58,14 +58,16 @@ namespace clientWpf
             }));
         }*/
 
-        private async void login_register()
+        private async void openLogin()
         {
-           
+            //LoginWindow lw = new LoginWindow();
             bool loginAuthorized = false;
             bLogOut.IsEnabled = false;
             //this.Username = settingsManager.readSetting("account", "username");
             //this.Password = settingsManager.readSetting("account", "password");
-            
+            while (!loginAuthorized)
+            {
+                //lw.showLogin();
                 try
                 {
                     switch (waitResponse())
@@ -107,6 +109,7 @@ namespace clientWpf
                         bStart.IsEnabled = true;
                         loggedin = true;
                         updateStatus("Logged in");
+                        //StartSync_Click(null, null); // start sync
                     }
                 }
                 catch (Exception ex)
@@ -114,7 +117,7 @@ namespace clientWpf
                     this.ErrorMessage = ex.Message;
                     loginAuthorized = false;
                 }
-            
+            }
             bLogOut.IsEnabled = true;
         }
 
@@ -174,7 +177,7 @@ namespace clientWpf
                 return;
             }
             lastResponse = LoginResponse.LOGIN;
-            this.login_register();
+            this.openLogin();
 
         }
 
@@ -188,7 +191,7 @@ namespace clientWpf
                 return;
             }
             lastResponse = LoginResponse.REGISTER;
-            this.login_register();
+            this.openLogin();
 
         }
 
